69128b2fa9ca8645efb612748ff2379bbaff0bae
DioBrandoPB <64975396+DioBrandoPB@users.noreply.github.com>
2020-06-16 00:06:41 +0200

    Mise a jour index
    Maj Index

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <!-- /.container -->
     <section id="Billet">
         <div class="container-fluid ">
-
+azezaeaz
             <h3>BIENVENUE SUR MON SITE </h3>
             <p>
                 Je suis Jean Forteroche, auteur de Roman vivant à Paris. Sur ce site Vous trouvez mon Blog avec ses différents chapitres, mes autres ouvrages, ma biographie ainsi qu'une page de contact si vous avez des questions.
